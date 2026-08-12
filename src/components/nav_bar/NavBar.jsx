c705b2c1cdc2d8a8f3d7528fc04f55c6daef2b0a
Deepika Chauhan <deepikachauhan8988@gmail.com>
2026-08-12 13:40:46 +0530

    updated

--- src/components/nav_bar/NavBar.jsx
+++ src/components/nav_bar/NavBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navbar, Nav, Button, Container, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { FaDatabase } from "react-icons/fa";
+import ulLogo from "../../assets/images/uk_logo.jpeg"
 
 function NavBar() {
   return (
@@ -9,7 +10,12 @@ function NavBar() {
       <Container>
         {/* Logo */}
         <Navbar.Brand as={Link} to="/">
-          <FaDatabase className="me-2" />
+          <img
+            src={ulLogo}
+            height="30"
+            className="d-inline-block align-top me-2"
+            alt="DRC Logo"
+          />
           Data Resource Center
         </Navbar.Brand>
 
