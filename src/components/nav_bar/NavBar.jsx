d7464071103091dfc692b2c200e9c30b83751ff4
Deepika Chauhan <deepikachauhan8988@gmail.com>
2026-08-12 13:25:45 +0530

    updated

--- src/components/nav_bar/NavBar.jsx
+++ src/components/nav_bar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Button, Container } from "react-bootstrap";
+import { Navbar, Nav, Button, Container, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { FaDatabase } from "react-icons/fa";
 
@@ -7,6 +7,7 @@ function NavBar() {
   return (
     <Navbar expand="lg" className="drc-navbar" sticky="top">
       <Container>
+        {/* Logo */}
         <Navbar.Brand as={Link} to="/">
           <FaDatabase className="me-2" />
           Data Resource Center
@@ -16,11 +17,75 @@ function NavBar() {
 
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
-              Home
+
+            {/* HOME DROPDOWN */}
+            <NavDropdown title="Home" id="home-dropdown">
+              <NavDropdown.Item as={Link} to="/">
+                Home
+              </NavDropdown.Item>
+
+              <NavDropdown.Item as={Link} to="/about">
+                About Us
+              </NavDropdown.Item>
+
+             
+            </NavDropdown>
+
+            {/* DEPARTMENTS */}
+            <NavDropdown title="Departments" id="departments-dropdown">
+              <NavDropdown.Item as={Link} to="/departments">
+                All Departments
+              </NavDropdown.Item>
+
+              <NavDropdown.Item as={Link} to="/departments/education">
+                Education Department
+              </NavDropdown.Item>
+
+              <NavDropdown.Item as={Link} to="/departments/health">
+                Health Department
+              </NavDropdown.Item>
+
+              <NavDropdown.Item as={Link} to="/departments/agriculture">
+                Agriculture Department
+              </NavDropdown.Item>
+
+              <NavDropdown.Item as={Link} to="/departments/welfare">
+                Social Welfare Department
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            {/* SCHEMES */}
+            <NavDropdown title="Schemes" id="schemes-dropdown">
+              <NavDropdown.Item as={Link} to="/schemes">
+                All Schemes
+              </NavDropdown.Item>
+
+              <NavDropdown.Item as={Link} to="/schemes/education">
+                Education Schemes
+              </NavDropdown.Item>
+
+              <NavDropdown.Item as={Link} to="/schemes/health">
+                Health Schemes
+              </NavDropdown.Item>
+
+              <NavDropdown.Item as={Link} to="/schemes/scholarship">
+                Scholarships
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            {/* SERVICES */}
+            <Nav.Link as={Link} to="/services">
+              Services
             </Nav.Link>
+
+            {/* BENEFITS */}
+            <Nav.Link as={Link} to="/benefits">
+              Benefits
+            </Nav.Link>
+
           </Nav>
 
+          {/* LOGIN */}
           <Nav>
             <Button
               as={Link}
@@ -32,6 +97,7 @@ function NavBar() {
               Login
             </Button>
           </Nav>
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
