a74ec090a342ac1d1176dd3db46f529eb36f8705
Deepika Chauhan <deepikachauhan8988@gmail.com>
2026-08-13 10:13:56 +0530

    updated

--- src/components/nav_bar/NavBar.jsx
+++ src/components/nav_bar/NavBar.jsx
@@ -24,69 +24,19 @@ function NavBar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
 
-            {/* HOME DROPDOWN */}
-            <NavDropdown title="Home" id="home-dropdown">
-              <NavDropdown.Item as={Link} to="/">
-                Home
-              </NavDropdown.Item>
-
-              <NavDropdown.Item as={Link} to="/about">
-                About Us
-              </NavDropdown.Item>
-
-             
-            </NavDropdown>
-
-            {/* DEPARTMENTS */}
-            <NavDropdown title="Departments" id="departments-dropdown">
-              <NavDropdown.Item as={Link} to="/departments">
-                All Departments
-              </NavDropdown.Item>
-
-              <NavDropdown.Item as={Link} to="/departments/education">
-                Education Department
-              </NavDropdown.Item>
-
-              <NavDropdown.Item as={Link} to="/departments/health">
-                Health Department
-              </NavDropdown.Item>
-
-              <NavDropdown.Item as={Link} to="/departments/agriculture">
-                Agriculture Department
-              </NavDropdown.Item>
-
-              <NavDropdown.Item as={Link} to="/departments/welfare">
-                Social Welfare Department
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            {/* SCHEMES */}
-            <NavDropdown title="Schemes" id="schemes-dropdown">
-              <NavDropdown.Item as={Link} to="/schemes">
-                All Schemes
-              </NavDropdown.Item>
-
-              <NavDropdown.Item as={Link} to="/schemes/education">
-                Education Schemes
-              </NavDropdown.Item>
-
-              <NavDropdown.Item as={Link} to="/schemes/health">
-                Health Schemes
-              </NavDropdown.Item>
-
-              <NavDropdown.Item as={Link} to="/schemes/scholarship">
-                Scholarships
-              </NavDropdown.Item>
-            </NavDropdown>
+            
 
             {/* SERVICES */}
             <Nav.Link as={Link} to="/services">
-              Services
+              Home
             </Nav.Link>
 
             {/* BENEFITS */}
             <Nav.Link as={Link} to="/AdminDashboard">
-              Benefits
+              About Us
+            </Nav.Link>
+            <Nav.Link as={Link} to="/AdminDashboard">
+              success story
             </Nav.Link>
 
           </Nav>
