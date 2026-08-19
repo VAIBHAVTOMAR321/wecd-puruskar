97f9cd04c8194f92235db2a3985ee7fe7d03f944
vibhu1900 <95899330+VAIBHAVTOMAR321@users.noreply.github.com>
2026-08-19 11:27:17 +0530

    new changes added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import ScrollToTop from "./components/ScrollToTop";
 import { AuthProvider } from "./components/login/AuthContext";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,6 +12,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router basename="/srcproject">
+      <ScrollToTop />
       <AuthProvider>
         <App />
       </AuthProvider>
